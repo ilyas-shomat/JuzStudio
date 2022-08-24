d710c4bc1e18e65ac31ad819a061e4c36ad464a4
Ilyas Shomat <ilyas.shomat@gmail.com>
2022-08-24 16:18:43 +0600

    Initial Commit

---
+++ JuzStudio/JuzStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuzStudioApp.swift
+//  JuzStudio
+//
+//  Created by Ilyas Shomat on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct JuzStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
